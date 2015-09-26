a10097dbb4d53a919a90fbfd526ffbf2181aea95
Kasper Holbek Jensen <khj@autobutler.dk>
2015-09-26 10:28:53 +0200

    add port configuration

--- lib/tasks/server.ex
+++ lib/tasks/server.ex
@@ -2,7 +2,8 @@ defmodule Mix.Tasks.Server do
   use Mix.Task
 
   def run(_) do
-    port = 8090
+    {port, _} = Integer.parse(System.get_env("PORT"))
+    port = port || 8090
     {:ok, _} = Plug.Adapters.Cowboy.http PokerPlayerElixir.AppRouter, [], port: port
     Mix.shell.info "Server started on port #{port} (Ctrl+C to abort)"
     :timer.sleep(:infinity)
